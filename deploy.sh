912aff4fc9487e9efdd027632378d690a3cda436
kinson <swilkinsonhunter@gmail.com>
2021-04-18 00:01:03 -0600

    update deploy script to only kill this webserver process

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ mix phx.digest
 mix compile
 mix ecto.migrate
 
-pkill -f "SpotMeProcess"
+kill -9 $(lsof -t -i:5000)
 
-bash -c "PORT=5000 exec -a SpotMeProcess elixir --erl "-detached" -S mix phx.server"
+PORT=5000 elixir --erl "-detached" -S mix phx.server
